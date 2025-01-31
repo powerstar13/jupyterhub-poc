fcaa930a57cdd76e09fa63bb37cb1630e8186335
홍준성 <jshong@sdt.inc>
2025-01-31 20:01:07 +0900

    JupyterHub > Keycloak 연동

--- PKCE.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-import base64
-
-code_challenge = "S7A1MBFvWGXhY6PLWPh0yMiqo-TBkp5Tdl9_4jegI2w"
-code_verifier = base64.urlsafe_b64encode(hashlib.sha256(code_challenge.encode()).digest()).decode().rstrip("=")
-
-print("code_challenge:", code_challenge)
-print("code_verifier:", code_verifier)
